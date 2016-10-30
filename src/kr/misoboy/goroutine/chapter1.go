e11e03dc5e974c722f93822895c8dcd81d4ccab3
misoboy <misoboy.kor@gmail.com>
2016-10-30 18:17:27 +0900

    1. Import Package 변경 2. test unit 추가

--- src/kr/misoboy/goroutine/chapter1.go
+++ src/kr/misoboy/goroutine/chapter1.go
@@ -1,4 +1,4 @@
-package goroutine
+package main
 
 import (
 	"fmt"
@@ -6,8 +6,18 @@ import (
 	"time"
 )
 
-func Hello(n int){
+func Hello(n int) {
 	r := rand.Intn(100)
 	time.Sleep(time.Duration(r))
 	fmt.Println(n)
+}
+
+func main() {
+
+	for i := 0; i < 100; i++ {
+		go Hello(i)
+	}
+
+	fmt.Scanln()
+	fmt.Println("Enter Exit")
 }
